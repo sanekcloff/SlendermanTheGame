e9a2201037c6f73073c6fd1e87c4abdb35d5fc7a
sanekcl <91375266+sanekcloff@users.noreply.github.com>
2026-01-26 08:03:33 +0300

    Add project files.

---
+++ Source/SlendermanTheGame/SlendermanTheGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SlendermanTheGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SlendermanTheGame, "SlendermanTheGame" );
